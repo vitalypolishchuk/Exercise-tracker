50826d3cf11c732cb98bcfe259da924bd7b87f0c
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-07-30 13:29:15 +0300

    'app class'

--- script.js
+++ script.js
@@ -9,63 +9,101 @@ const elevGain = document.getElementById("elev-gain");
 const field4 = document.querySelector(".field-4"); // cadence container
 const field5 = document.querySelector(".field-5"); // elevGain container
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
 let map;
 let mapEvent;
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
+/////////////// DATA ///////////////
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-      map = L.map("map").setView(coords, 13);
+class App {
+  #map;
+  #mapEvent;
 
-      googleStreets = L.tileLayer("http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}", {
-        maxZoom: 20,
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-      });
-      googleStreets.addTo(map);
+  constructor() {
+    this._getPosition();
 
-      // set new coordinates when clicking on the map
-      map.on("click", function (mapE) {
-        mapEvent = mapE;
-        inputContainer.classList.remove("hidden");
-      });
-    },
-    function () {
-      alert("Could not get your position!");
-    }
-  );
-
-form.addEventListener("keypress", function (e) {
-  if (e.key !== "Enter") return;
-  if (!distance.value || !duration.value || !cadence.value) return alert("Please fill all the fields!");
-  if (Number(distance.value) <= 0 || Number(duration.value) <= 0 || Number(cadence.value) <= 0) return alert("Please provide positive values!");
-  distance.value = duration.value = cadence.value = "";
-
-  const { lat, lng } = mapEvent.latlng;
-  map.setView([lat, lng], 13);
-  console.log(L);
-  L.marker([lat, lng], {})
-    .addTo(map)
-    .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "running-popup" }))
-    .setPopupContent("Workout")
-    .openPopup();
-});
-exerType.addEventListener("change", function (e) {
-  if (exerType.value === "run") {
-    field5.classList.add("none");
-    field4.classList.remove("none");
-  } else {
-    field4.classList.add("none");
-    field5.classList.remove("none");
+    form.addEventListener("keypress", this._newWorkout.bind(this));
+    exerType.addEventListener("change", this._toggleElevationField.bind(this));
   }
-});
 
-// window.addEventListener("keypress", function (e) {
-//   if(e.key === 'Enter')
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+        alert("Could not get your position!");
+      });
+  }
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-// });
+    this.#map = L.map("map").setView(coords, 13);
+
+    const googleStreets = L.tileLayer("http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}", {
+      maxZoom: 20,
+      subdomains: ["mt0", "mt1", "mt2", "mt3"],
+    });
+    googleStreets.addTo(this.#map);
+
+    this.#map.on("click", this._showForm.bind(this));
+  }
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    inputContainer.classList.remove("hidden");
+    console.log(this.#mapEvent);
+  }
+  _toggleElevationField() {
+    field4.classList.toggle("none");
+    field5.classList.toggle("none");
+  }
+  _newWorkout(e) {
+    if (e.key !== "Enter") return;
+    if (!distance.value || !duration.value || (exerType.value === "run" && !cadence.value) || (exerType.value === "cyc" && !elevGain.value))
+      return alert("Please fill all the fields!");
+    if (
+      Number(distance.value) <= 0 ||
+      Number(duration.value) <= 0 ||
+      (exerType.value === "run" && Number(cadence.value) <= 0) ||
+      (exerType.value === "cyc" && Number(elevGain.value) <= 0)
+    )
+      return alert("Please provide positive values!");
+
+    distance.value = duration.value = cadence.value = "";
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    this.#map.setView([lat, lng], 13);
+    L.marker([lat, lng], {})
+      .addTo(this.#map)
+      .bindPopup(L.popup({ maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: "running-popup" }))
+      .setPopupContent("Workout")
+      .openPopup();
+  }
+}
+const app = new App();
+
+class Workout {
+  #id;
+  constructor(id, distance, duration, coords, date) {
+    this.#id = id;
+    this.distance = distance;
+    this.duration = duration;
+    this.coords = coords;
+    this.date = date;
+  }
+}
+class Running extends Workout {
+  constructor(id, distance, duration, coords, date, name, cadence, pace) {
+    super(id, distance, duration, coords, date);
+    this.name = name;
+    this.cadence = cadence;
+    this.pace = pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(id, distance, duration, coords, date, name, elevationGain, speed) {
+    super(id, distance, duration, coords, date);
+    this.name = name;
+    this.elevationGain = elevationGain;
+    this.speed = speed;
+  }
+}
