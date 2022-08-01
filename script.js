5f79fdb5c34645d4b702558e14d1d4ce7c7d0ef2
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-08-01 20:26:23 +0300

    'workout editor'

--- script.js
+++ script.js
@@ -10,9 +10,26 @@ const elevGain = document.getElementById("elev-gain");
 const field4 = document.querySelector(".field-4"); // cadence container
 const field5 = document.querySelector(".field-5"); // elevGain container
 
+/////////////// EDITOR ///////////////
+const markerEditor = document.querySelector(".marker-editor");
+const editInput = document.querySelector(".edit-input-container");
+const overlay = document.querySelector(".overlay");
+const distanceEdit = document.getElementById("distance-edit");
+const durationEdit = document.getElementById("duration-edit");
+const cadenceEdit = document.getElementById("cadence-edit");
+const elevGainEdit = document.getElementById("elev-gain-edit");
+const dateEdit = document.getElementById("date-edit");
+const field4Edit = document.querySelector(".field-4-edit"); // cadence container
+const field5Edit = document.querySelector(".field-5-edit"); // elevGain container
+const cancelBtn = document.querySelector(".cancel");
+const saveBtn = document.querySelector(".save");
+
+// localStorage.clear();
+
 /////////////// DATA ///////////////
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const zoomMap = 13;
+let x;
 
 /////////////// WORKOUT CLASS ///////////////
 class Workout {
@@ -149,16 +166,22 @@ class App {
       .openPopup();
     workout.marker = marker; // add marker to workout
   }
-  _renderWorkout(workout) {
+  _stringToHTML = function (str) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(str, "text/html");
+    return doc.body.children[0];
+  };
+  _renderWorkout(workout, workoutIndex) {
     const htmlRunning = `
       <div class="workout ${workout.type}" data-id="${workout.id}">
         <h3>Running on ${months[workout.date.getMonth()]} ${workout.date.getDate()}</h3>
         <div class="stat-inner-container">
-          <h4>🏃‍♂️ ${workout.distance} <span class="unit">KM </span></h4>
-          <h4>⏱ ${workout.duration} <span class="unit">MIN </span></h4>
-          <h4>⚡️ ${workout.pace} <span class="unit">MIN/KM </span></h4>
-          <h4>🦶🏼 ${workout.cadence} <span class="unit">SPM </span></h4>
+          <h4 class="run-distance">🏃‍♂️ ${workout.distance} <span class="unit">KM </span></h4>
+          <h4 class="run-duration">⏱ ${workout.duration} <span class="unit">MIN </span></h4>
+          <h4 class="run-pace">⚡️ ${workout.pace} <span class="unit">MIN/KM </span></h4>
+          <h4 class="run-cadence">🦶🏼 ${workout.cadence} <span class="unit">SPM </span></h4>
         </div>
+        <span class="edit none"><i class="fa-solid fa-pen-to-square"></i></span>
         <span class="delete none"><i class="fa-solid fa-circle-xmark"></i></span>
       </div>
     `;
@@ -166,39 +189,115 @@ class App {
       <div class="workout ${workout.type}" data-id="${workout.id}">
         <h3>Cycling on ${months[workout.date.getMonth()]} ${workout.date.getDate()}</h3>
         <div class="stat-inner-container">
-          <h4>🚴‍♀️ ${workout.distance} <span class="unit">KM </span></h4>
-          <h4>⏱ ${workout.duration} <span class="unit">MIN </span></h4>
-          <h4>⚡️ ${workout.speed} <span class="unit">KM/H </span></h4>
-          <h4>⛰ ${workout.elevationGain} <span class="unit">M </span></h4>
+          <h4 class="cyc-distance">🚴‍♀️ ${workout.distance} <span class="unit">KM </span></h4>
+          <h4 class="cyc-duration">⏱ ${workout.duration} <span class="unit">MIN </span></h4>
+          <h4 class="cyc-speed">⚡️ ${workout.speed} <span class="unit">KM/H </span></h4>
+          <h4 class="cyc-elevationGain">⛰ ${workout.elevationGain} <span class="unit">M </span></h4>
         </div>
+        <span class="edit none"><i class="fa-solid fa-pen-to-square"></i></span>
         <span class="delete none"><i class="fa-solid fa-circle-xmark"></i></span>
       </div>
     `;
     const insertHtml = workout.type === "Running" ? htmlRunning : htmlCycling;
-    infoContainer.insertAdjacentHTML("afterbegin", insertHtml);
-
+    if (typeof workoutIndex === "undefined") {
+      infoContainer.insertAdjacentHTML("afterbegin", insertHtml);
+    } else {
+      infoContainer.replaceChild(this._stringToHTML(insertHtml), infoContainer.children[workoutIndex]);
+    }
+    // console.log([...infoContainer.children]);
     this._showWorkouts();
   }
   _moveMapToWorkout(e) {
-    if (e.target.parentElement.classList.contains("delete")) return;
     const workoutElement = e.target.closest(".workout");
     if (!workoutElement) return;
     const workout = this.#workouts.find((workout) => workout.id === workoutElement.getAttribute("data-id"));
     this.#map.setView(workout.coords, this.#map.getZoom(), { animate: true, pan: { duration: 1 } });
 
+    // delete workout
     [...document.querySelectorAll(".delete")].forEach((dltBtn) => dltBtn.classList.add("none"));
     const deleteBtn = [...workoutElement.children].find((child) => child.classList.contains("delete"));
     deleteBtn.classList.remove("none");
-    deleteBtn.addEventListener("click", this._removeWorkout.bind(this, workout));
+    deleteBtn.addEventListener(
+      "click",
+      function (e) {
+        this._removeWorkout(e, workout);
+      }.bind(this)
+    );
+
+    // edit workout
+    [...document.querySelectorAll(".edit")].forEach((editBtn) => editBtn.classList.add("none"));
+    const editBtn = [...workoutElement.children].find((child) => child.classList.contains("edit"));
+    editBtn.classList.remove("none");
+    editBtn.addEventListener(
+      "click",
+      function (e) {
+        this._editWorkout(e, workout);
+      }.bind(this)
+    );
   }
-  _removeWorkout(workout) {
-    // console.log(workout);
+  _removeWorkout(e, workout) {
+    e.stopImmediatePropagation();
     this.#map.removeLayer(workout.marker); // remove marker
     const workoutIndex = this.#workouts.findIndex((wrk) => wrk.id === workout.id);
     infoContainer.removeChild(infoContainer.children[infoContainer.children.length - 1 - workoutIndex]); // remove HTML
     localStorage.removeItem(workout.id); // remove from storage
     this.#workouts.splice(workoutIndex, 1); // remove from []
     this._showWorkouts.call(this);
+  }
+  _removeEditFields() {
+    markerEditor.classList.add("none");
+    overlay.classList.add("none");
+    field4Edit.classList.add("none");
+    field5Edit.classList.add("none");
+  }
+  _editWorkout(e, workout) {
+    e.stopImmediatePropagation();
+    markerEditor.classList.remove("none");
+    overlay.classList.remove("none");
+    if (workout.type === "Running") field4Edit.classList.remove("none");
+    else field5Edit.classList.remove("none");
+    cancelBtn.addEventListener("click", this._removeEditFields);
+    x = this._addEvent.bind(this, e, workout);
+    saveBtn.addEventListener("click", x);
+  }
+  _addEvent(e, workout) {
+    saveBtn.removeEventListener("click", x);
+    this._editWorkout2(e, workout);
+  }
+  _editWorkout2(e, workout) {
+    e.stopImmediatePropagation();
+    console.log(workout);
+    if (
+      !distanceEdit.value ||
+      !durationEdit.value ||
+      (workout.type === "Running" && !cadenceEdit.value) ||
+      (workout.type === "Cycling" && !elevGainEdit.value)
+    )
+      return alert("Please fill all the fields!");
+    if (Number(distanceEdit.value) <= 0 || Number(durationEdit.value) <= 0 || (workout.type === "Running" && Number(cadenceEdit.value) <= 0))
+      return alert("Please provide positive values!");
+
+    workout.distance = Number(distanceEdit.value);
+    workout.duration = Number(durationEdit.value);
+    if (workout.type === "Running") {
+      workout.cadence = Number(cadenceEdit.value);
+      workout.pace = Math.round((workout.duration / workout.distance) * 100) / 100;
+    } else {
+      workout.elevationGain = Number(elevGainEdit.value);
+      workout.speed = Math.round((workout.distance / (workout.duration / 60)) * 100) / 100;
+    }
+
+    workout.date = new Date(dateEdit.value);
+
+    distanceEdit.value = "";
+    durationEdit.value = "";
+    cadenceEdit.value = "";
+    elevGainEdit.value = "";
+
+    const workoutIndex = [...infoContainer.children].findIndex((wrk) => wrk.getAttribute("data-id") === workout.id);
+    this._renderWorkout(workout, workoutIndex);
+
+    this._removeEditFields();
   }
   _setLocalStorage() {
     const objForStorage = this.#workouts.map((workout) => JSON.stringify(workout, getCircularReplacer()));
@@ -213,6 +312,11 @@ class App {
       this.#workouts.push(workout);
       this._renderWorkout(workout);
     }
+  }
+  _showAllMarkers() {
+    const markerArray = this.#workouts.map((workout) => workout.coords);
+    const bounds = L.latLngBounds(markerArray);
+    this.#map.fitBounds(bounds);
   }
 }
 const app = new App();
